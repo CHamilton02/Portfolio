3278a4c2bb4974eb6d5aa5dfa9b5c03f4116caf4
Camdyn Hamilton <c02hamil@gmail.com>
2024-09-29 11:12:41 -0400

    refactor: move navbar into it's own component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav class="navbar">
+    <div class="name-and-icon">
+      <img :src="RightIndex" class="icon" />
+      <h1 class="name">Camdyn Hamilton</h1>
+    </div>
+    <div class="sections">
+      <h2 v-for="section in sections" class="section">{{ section }}</h2>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import RightIndex from "../assets/index-pointing-right.png";
+import { ref } from "vue";
+
+const sections = ref(["Home", "About", "Projects", "Contact Me"]);
+</script>
+
+<style>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  background-color: #685343;
+}
+.name-and-icon {
+  display: flex;
+  margin-left: 0.5rem;
+  align-items: center;
+}
+.icon {
+  width: auto;
+  height: 2rem;
+  padding-right: 0.5rem;
+}
+.name {
+}
+.sections {
+  display: flex;
+  margin-right: 0.5rem;
+}
+.section {
+  margin-left: 1rem;
+}
+.section:hover {
+  color: #FFE2D6;
+  cursor: pointer;
+}
+</style>
