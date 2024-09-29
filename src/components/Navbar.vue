07ca94940dc9b4ceb01ae9b873c42f24bfd28cee
Camdyn Hamilton <c02hamil@gmail.com>
2024-09-29 12:06:41 -0400

    feat: add mouse hover effect on name

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,11 @@
 <template>
   <nav>
-    <div class="name-and-icon">
+    <div class="name-and-icon" @mouseover="japaneseName = true" @mouseleave="japaneseName = false">
       <img :src="RightIndex" class="icon" />
-      <h1 class="name">Camdyn Hamilton</h1>
+      <Transition name="slide-fade" mode="out-in">
+        <h1 class="name" v-if="japaneseName">ハミルトンキャムデン</h1>
+        <h1 class="name" v-else>Camdyn Hamilton</h1>
+      </Transition> 
     </div>
     <div class="sections">
       <h2 v-for="section in sections" class="section">{{ section }}</h2>
@@ -15,6 +18,7 @@ import RightIndex from "../assets/index-pointing-right.png";
 import { ref } from "vue";
 
 const sections = ref(["Home", "About", "Projects", "Contact Me"]);
+const japaneseName = ref(false);
 </script>
 
 <style>
@@ -25,7 +29,9 @@ nav {
   left: 0;
   right: 0;
   top: 0;
-  background-color: #685343;
+  /* background-color: #685343; */
+  height: 5rem;
+  align-items: center;
 }
 .name-and-icon {
   display: flex;
@@ -37,17 +43,29 @@ nav {
   height: 2rem;
   padding-right: 0.5rem;
 }
-.name {
-}
 .sections {
   display: flex;
   margin-right: 0.5rem;
 }
 .section {
   margin-left: 1rem;
+  transition: color 0.25s;
 }
 .section:hover {
   color: #FFE2D6;
   cursor: pointer;
+}
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(-10px);
+  opacity: 0;
 }
 </style>
