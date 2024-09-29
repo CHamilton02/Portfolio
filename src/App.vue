1a77d57acb0a4ef55ca0f2edd7eb64133d5072d6
Camdyn Hamilton <c02hamil@gmail.com>
2024-09-29 12:28:23 -0400

    feat: create about section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Navbar />
+  <About />
 </template>
 
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue'
+import About from './components/About.vue'
 </script>
 
 <style scoped>
---
+++ src/components/About.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="about">
+    <div>
+      <h1>Hello, I'm Camdyn!👋</h1>
+      <h2>Student/Full-Stack Developer 💻</h2>
+      <p>Currently working at RBC as a Developer co-op on the UX-Force team! 🏦</p>
+      <p>I'm also a Computer Science student at York University. 🏫</p>
+    </div>
+    <div class="about__profile-picture">
+      <img v-if="!showCat" src="../assets/Profile Picture.jpg" class="profile-picture" @click="showCat = true"/>
+      <img v-else src="../assets/Totoro.png" class="profile-picture" @click="showCat = false"/>
+      <p>(click image for a surprise 🐈)</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const showCat = ref(false)
+</script>
+
+<style lang="scss" scoped>
+.about {
+  margin-top: 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &__profile-picture {
+    margin-left: 5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+.profile-picture {
+  height: 10rem;
+  width: auto;
+  object-fit: cover;
+  border-radius: 50%;
+}
+</style>
