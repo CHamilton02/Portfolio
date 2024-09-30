3bec84c169ceac349905c10c0d65c475ff5bcc4e
Camdyn Hamilton <c02hamil@gmail.com>
2024-09-30 13:19:29 -0400

    fix: temporarily remove svg navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,20 +14,6 @@
     <div class="sections">
       <h2 v-for="section in sections" class="section">{{ section }}</h2>
     </div>
-    <svg
-      id="visual"
-      viewBox="0 0 900 600"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-      version="1.1"
-    >
-      <path
-        d="M0 40L18.8 46.5C37.7 53 75.3 66 112.8 73.5C150.3 81 187.7 83 225.2 84.2C262.7 85.3 300.3 85.7 337.8 78.2C375.3 70.7 412.7 55.3 450.2 55C487.7 54.7 525.3 69.3 562.8 71.3C600.3 73.3 637.7 62.7 675.2 54C712.7 45.3 750.3 38.7 787.8 41.5C825.3 44.3 862.7 56.7 881.3 62.8L900 69L900 0L881.3 0C862.7 0 825.3 0 787.8 0C750.3 0 712.7 0 675.2 0C637.7 0 600.3 0 562.8 0C525.3 0 487.7 0 450.2 0C412.7 0 375.3 0 337.8 0C300.3 0 262.7 0 225.2 0C187.7 0 150.3 0 112.8 0C75.3 0 37.7 0 18.8 0L0 0Z"
-        fill="#413429"
-        stroke-linecap="round"
-        stroke-linejoin="miter"
-      ></path>
-    </svg>
   </nav>
 </template>
 
@@ -84,13 +70,5 @@ nav {
 .slide-fade-leave-to {
   transform: translateX(-10px);
   opacity: 0;
-}
-svg {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  width: 100%;
-  z-index: -1;
 }
 </style>
