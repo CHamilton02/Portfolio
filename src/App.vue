beab32c2a888d18e923b6ec9748ab38d1d42c309
Camdyn Hamilton <c02hamil@gmail.com>
2024-10-02 21:55:46 -0400

    refactor: move navbar background style to style.css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <div class="spacer layer1"></div>
   <Navbar />
   <About />
 </template>
@@ -10,16 +9,4 @@ import About from './components/About.vue'
 </script>
 
 <style scoped>
-  .spacer {
-    aspect-ratio: 3480/300;
-    width: 100%;
-    height: 350px;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-  }
-
-  .layer1 {
-    background-image: url('./assets/navbar-waves.svg');
-  }
 </style>
