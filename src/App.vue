4165f3331bab2d691fa07489104d70123d6487e9
Camdyn Hamilton <c02hamil@gmail.com>
2024-10-08 17:13:58 -0400

    feat: ensure background is always covered by animation

--- src/App.vue
+++ src/App.vue
@@ -21,10 +21,13 @@ onMounted(() => {
 <style scoped>
 .background {
   z-index: -1;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
 }
 </style>
