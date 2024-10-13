a04cc8d58b586c74b7eaee988d8a2767d9e01eb3
Camdyn Hamilton <c02hamil@gmail.com>
2024-10-12 22:34:58 -0400

    feat: make ExperienceCard reusable and add additional experiences

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,22 +1,61 @@
 <template>
-    <div class="experience-card-container">
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
-    </div>
+  <div class="experience-card-container">
+    <ExperienceCard v-for="experience in experiences" :experience />
+  </div>
 </template>
 
 <script setup lang="ts">
-import ExperienceCard from './ExperienceCard.vue'
+import { ref } from "vue";
+import ExperienceCard from "./ExperienceCard.vue";
+
+const experiences = ref([
+  {
+    date: "SEPT. 2024 - PRESENT",
+    title: "Developer Co-Op",
+    organization: "RBC",
+    description:
+      "Build and maintain critical components used to construct front-end components for various artificial intelligence and machine learning projects. Work closely in an agile environment with a cross-functional team consisting of designers, developers, and product managers to ensure web accessibility.",
+    skills: ["JavaScript", "TypeScript", "Vue", "Pinia"],
+  },
+  {
+    date: "OCT. 2024 - PRESENT",
+    title: "Portfolio",
+    description:
+      "Created to showcase my various talents in the Software Development field. Developed using TypeScript and Vue, along with various JavaScript animation libraries.",
+    skills: ["TypeScript", "Vue", "Typeit", "Vanta"],
+  },
+  {
+    date: "JUL. 2024",
+    title: "SportIQs",
+    description: "A Sports trivia website (made with React)",
+    skills: ["JavaScript", "React"],
+  },
+  {
+    date: "MAY 2024 - JUN. 2024",
+    title: "Djibouti",
+    description:
+      "Produced an interactive web game using HTML, CSS, and JavaScript, inspired by the popular online game Wordle, with a unique country theme.",
+    skills: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    date: "MAR. 2024 - MAY 2024",
+    title: "Front-End Developer",
+    organization: "Stone Row Farm Inc.",
+    description:
+      "Developed various business web pages by creating and maintaining code using HTML/CSS, Bootstrap, and JavaScript. Communicated with company owners to gather feedback and refine interface designs, proposing innovative solutions to meet evolving requirements.",
+    skills: ["HTML", "CSS", "Bootstrap"],
+  },
+]);
 </script>
 
 <style lang="scss">
 .experience-card-container {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center;
-    padding-bottom: 1rem;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  // justify-content: space-evenly;
+  gap: 2rem;
+  align-items: center;
+  padding-bottom: 1rem;
 }
 </style>
--- src/components/ExperienceCard.vue
+++ src/components/ExperienceCard.vue
@@ -1,72 +1,80 @@
 <template>
-    <div class="experience-card">
-        <div class="experience-card__dates">SEPT. 2024 - PRESENT</div>
-        <div class="experience-card__details">
-            <h2 class="experience-card__details__title">
-                Developer Co-Op, <span class="experience-card__details__title__company">RBC</span>
-            </h2>
-            <p class="experience-card__details__description">
-                Build and maintain critical components used to construct front-end components for various artificial
-                intelligence and machine learning projects. Work closely in an agile environment with a cross-functional
-                team consisting of designers, developers, and product managers to ensure web accessibility.
-            </p>
-            <div class="experience-card__details__skills-container">
-                <button class="experience-card__details__skills-container__item">JavaScript</button>
-                <button class="experience-card__details__skills-container__item">TypeScript</button>
-                <button class="experience-card__details__skills-container__item">Vue</button>
-                <button class="experience-card__details__skills-container__item">Pinia</button>
-            </div>
-        </div>
+  <div class="experience-card">
+    <div class="experience-card__dates">{{ experience.date }}</div>
+    <div class="experience-card__details">
+      <h2 class="experience-card__details__title">
+        {{ experience.title }},
+        <span class="experience-card__details__title__company">{{
+          experience.organization ? experience.organization : "Personal Project"
+        }}</span>
+      </h2>
+      <p class="experience-card__details__description">
+        {{ experience.description }}
+      </p>
+      <div class="experience-card__details__skills-container">
+        <button
+          class="experience-card__details__skills-container__item"
+          v-for="skill in experience.skills"
+        >
+          {{ skill }}
+        </button>
+      </div>
     </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ExperienceDetails } from "../types/experience";
+
+defineProps({
+  experience: {
+    type: Object as () => ExperienceDetails,
+    required: true,
+  },
+});
+</script>
 
 <style lang="scss">
 .experience-card {
-    display: flex;
-    // background-color: #23153c;
-    // border: solid;
-    padding: 1rem;
-    border-color: #ff3f81;
-    border-radius: 1rem;
-    max-width: 50rem;
+  display: flex;
+  padding: 1rem;
+  width: 50rem;
 
-    &__dates {
-        font-size: 0.80rem;
-        min-width: 9rem;
-        margin-right: 2rem;
+  &__dates {
+    font-size: 0.8rem;
+    min-width: 9rem;
+    margin-right: 2rem;
+    color: #ff3f81;
+  }
+
+  &__details {
+    &__title {
+      margin: 0;
+      padding: 0;
+      font-size: 1.25rem;
+
+      &__company {
         color: #ff3f81;
+      }
     }
 
-    &__details {
-        &__title {
-            margin: 0;
-            padding: 0;
-            font-size: 1.25rem;
-
-            &__company {
-                color: #ff3f81;
-            }
-        }
-
-        &__description {
-            font-size: 0.9rem;
-        }
-
-        &__skills-container {
-            display: flex;
-            gap: 1rem;
-
-            &__item {
-                background-color: #ff3f81;
-                font-family: "Inter Tight", Arial, Helvetica, sans-serif;
-                border: none;
-                color: white;
-                padding: 0.5rem 1rem 0.5rem 1rem;
-                border-radius: 1rem;
-            }
-        }
+    &__description {
+      font-size: 0.9rem;
     }
+
+    &__skills-container {
+      display: flex;
+      gap: 1rem;
+
+      &__item {
+        background-color: #ff3f81;
+        font-family: "Inter Tight", Arial, Helvetica, sans-serif;
+        border: none;
+        color: white;
+        padding: 0.5rem 1rem 0.5rem 1rem;
+        border-radius: 1rem;
+      }
+    }
+  }
 }
 </style>
