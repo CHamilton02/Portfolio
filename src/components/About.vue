90c2e79596221478c947d0b5acf6f17c2a592f7c
Camdyn Hamilton <c02hamil@gmail.com>
2024-10-18 18:56:50 -0400

    chore: cleanup About component code

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,62 +1,50 @@
 <template>
   <div class="about">
     <h2 class="about__title">About Me!</h2>
-    <div class="about__container">
-      <p class="about__container__introduction">
+    <div class="about__body">
+      <p class="about__intro">
         Hey there! I'm Camdyn, and I love bringing ideas to life through code
         and teamwork!
       </p>
-      <div class="about__container__body">
-        <p class="about__container__body__text">
-          My coding journey kicked off in 2018, during my first computer
-          programming class in high school, and I’ve been hooked ever since.
-          I've worked on projects both academically and as a hobby, and now I’m
-          gaining real-world experience as a
-          <span class="about__container__body__text__highlight"
-            >Developer Co-op</span
-          >
-          at <span class="about__container__body__text__highlight">RBC</span>.
+      <div class="about__content">
+        <p class="about__text">
+          My coding journey kicked off in 2018 during my first computer
+          programming class, and I’ve been hooked since. I've worked on projects
+          both academically and as a hobby, and now I’m gaining real-world
+          experience as a
+          <span class="highlight">Developer Co-op</span> at
+          <span class="highlight">RBC</span>.
         </p>
-        <p class="about__container__body__text">
+        <p class="about__text">
           I’ve honed my front-end skills with technologies like
-          <span class="about__container__body__text__highlight">JavaScript</span
-          >,
-          <span class="about__container__body__text__highlight">TypeScript</span
-          >, <span class="about__container__body__text__highlight">React</span>,
-          and <span class="about__container__body__text__highlight">Vue</span>,
-          but my long-term dream is to dive into
-          <span class="about__container__body__text__highlight"
-            >operating systems</span
-          >
-          and eventually
-          <span class="about__container__body__text__highlight"
-            >lead a team to new heights</span
-          >.
+          <span class="highlight">JavaScript</span>,
+          <span class="highlight">TypeScript</span>,
+          <span class="highlight">React</span>, and
+          <span class="highlight">Vue</span>, but my long-term dream is to dive
+          into <span class="highlight">operating systems</span> and eventually
+          <span class="highlight">lead a team to new heights</span>.
         </p>
-        <p class="about__container__body__text">
-          So far, I've had the chance to deliver some exciting projects—whether
-          it’s personal creations like this
-          <span class="about__container__body__text__highlight">portfolio</span>
-          or
-          <span class="about__container__body__text__highlight"
+        <p class="about__text">
+          So far, I've had the chance to deliver exciting projects, whether
+          personal creations like this
+          <span class="highlight">portfolio</span> or
+          <span class="highlight"
             >building front-end interfaces to interact with AI/ML projects</span
           >
-          at RBC!
+          at RBC.
         </p>
-        <p class="about__container__body__text">
+        <p class="about__text">
           I’m always eager to grow and would be thrilled to bring my passion and
           skills to your team through an exciting co-op opportunity. Feel free
-          to reach out—<span class="about__container__body__text__highlight"
+          to reach out—<span class="highlight"
             >I'd love to chat about how I can contribute to your company!</span
           >
         </p>
-        <button
-          class="link-button"
-          target="_blank"
-          @click="$emit('toggleExperience')"
-        >
-          Check out what I've worked on!
-        </button>
+        <div class="about__cta">
+          <button class="btn" @click="$emit('toggleExperience')">
+            Check out what I've worked on!
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -66,73 +54,82 @@
 
 <style lang="scss" scoped>
 .about {
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 100vh;
-  margin-bottom: 2rem;
-  padding: 0 1rem 0 1rem;
+  padding: 0 1rem;
 
   &__title {
     font-size: 2.5rem;
+    margin-bottom: 2rem;
   }
 
-  &__container {
+  &__body {
     display: flex;
+    gap: 2rem;
+  }
 
-    &__introduction {
-      font-weight: bold;
-      font-size: 2rem;
-      margin: 0;
-    }
+  &__intro {
+    font-weight: bold;
+    font-size: 2rem;
+  }
 
-    &__body {
-      margin-left: 2rem;
+  &__content {
+    margin-top: 1rem;
+  }
 
-      &__text {
-        margin-top: 0;
+  &__text {
+    margin: 1rem 0;
+  }
 
-        &__highlight {
-          font-weight: bold;
-          color: #ff3f81;
-        }
-      }
-    }
+  .highlight {
+    font-weight: bold;
+    color: #ff3f81;
+  }
 
-    &__button {
-      font-family: "Inter Tight", Arial, Helvetica, sans-serif;
-      color: white;
-      background-color: #23153c;
-      text-decoration: none;
-      border: solid;
-      border-color: #ff3f81;
-      border-width: 0.15rem;
-      border-radius: 1rem;
-      padding: 0.5rem 1rem 0.5rem 1rem;
-      margin-top: 1rem;
-      transition: background-color 0.2s linear;
-    }
+  &__cta {
+    margin-top: 1rem;
+  }
 
-    &__button:hover {
+  .btn {
+    background-color: #23153c;
+    color: white;
+    padding: 0.5rem 1rem;
+    border: 0.15rem solid #ff3f81;
+    border-radius: 1rem;
+    transition: background-color 0.2s;
+
+    &:hover {
       background-color: #ff3f81;
       cursor: pointer;
     }
   }
 }
 
-@media only screen and (max-width: 40rem) {
-  .about__container {
-    display: block;
-    padding: 0 1rem 0 1rem;
-
-    &__introduction {
-      font-size: 1.25rem;
-    }
-
+@media (max-width: 640px) {
+  .about {
     &__body {
-      margin: 1rem 0 0 0;
+      display: block;
+      text-align: center;
     }
+
+    &__title {
+      font-size: 2rem;
+    }
+
+    &__intro {
+      font-size: 1.5rem;
+    }
+
+    &__cta {
+      text-align: center;
+    }
+  }
+
+  .btn {
+    font-size: 1rem;
   }
 }
 </style>
