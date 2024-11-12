f64dd67d118db3a97bcf01ebc89b05325db97ae4
Camdyn Hamilton <c02hamil@gmail.com>
2024-11-11 21:42:31 -0500

    Feat: limit experiences to four on the initial page, include view more and less toggle.

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -3,6 +3,7 @@
     <h2 class="header">My Experience</h2>
     <div class="experience-card-container">
       <ExperienceCard
+        v-if="experiencesLength <= 4"
         v-for="experience in experienceStore.experiences"
         :date="experience.date"
         :website="experience.website"
@@ -13,6 +14,40 @@
         :description="experience.description"
         :skills="experience.skills"
       />
+      <ExperienceCard
+        v-else
+        v-if="experienceStore.experiences"
+        v-for="i in numberOfViewableExperiences"
+        :date="experienceStore.experiences[i - 1].date"
+        :website="experienceStore.experiences[i - 1].website"
+        :code="experienceStore.experiences[i - 1].code"
+        :title="experienceStore.experiences[i - 1].title"
+        :organization="experienceStore.experiences[i - 1].organization"
+        :miniDescription="experienceStore.experiences[i - 1].miniDescription"
+        :description="experienceStore.experiences[i - 1].description"
+        :skills="experienceStore.experiences[i - 1].skills"
+      />
+    </div>
+    <div
+      v-if="numberOfViewableExperiences < experiencesLength"
+      class="toggle-experiences"
+    >
+      <button
+        class="toggle-experiences__button"
+        @click="toggleNumberOfViewableExperiences"
+      >
+        <img src="../assets/Down Arrow Icon.png" class="icon" />
+        View More
+      </button>
+    </div>
+    <div v-else class="toggle-experiences">
+      <button
+        class="toggle-experiences__button"
+        @click="toggleNumberOfViewableExperiences"
+      >
+        <img src="../assets/Up Arrow Icon.png" class="icon" />
+        View Less
+      </button>
     </div>
   </div>
 </template>
@@ -20,8 +55,27 @@
 <script setup lang="ts">
 import ExperienceCard from "./ExperienceCard.vue";
 import { useExperienceStore } from "../stores/experience";
+import { computed, ref } from "vue";
 
 const experienceStore = useExperienceStore();
+const experiencesLength = computed(() => {
+  if (experienceStore.experiences) {
+    return experienceStore.experiences.length;
+  }
+  return 0;
+});
+
+const numberOfViewableExperiences = ref(4);
+
+function toggleNumberOfViewableExperiences() {
+  if (numberOfViewableExperiences.value < experiencesLength.value - 1) {
+    numberOfViewableExperiences.value += 2;
+  } else if (numberOfViewableExperiences.value < experiencesLength.value) {
+    numberOfViewableExperiences.value += 1;
+  } else {
+    numberOfViewableExperiences.value = 4;
+  }
+}
 </script>
 
 <style lang="scss">
@@ -34,6 +88,22 @@ const experienceStore = useExperienceStore();
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 1rem;
+}
+
+.toggle-experiences {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__button {
+    background: none;
+    border: none;
+    display: flex;
+    color: #ff6f61;
+    align-items: center;
+    cursor: pointer;
+    font-size: 1rem;
+  }
 }
 
 @media only screen and (max-width: 56.25rem) {
